b23f73682626b660b44764c90f77f554aabd28bb
darma <darmayasa221@gmail.com>
2020-12-14 15:40:59 +0800

    testing function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,68 @@
 // import logo from './logo.svg';
-import { Component } from 'react';
-import './App.css';
-import PersonList from './PersonList'
-class App extends Component {
-  state={ test : 'test'}
-  render(){
-    return (
-      <div className="App">
-        <PersonList test={this.state.test}/>
-      </div>
-    );
-  }
-  
-}
+// import { Component } from 'react';
 
-export default App;
+// import PersonList from './PersonList'
+// class App extends Component {
+//   state={ test : 'test'}
+//   render(){
+//     return (
+//       <div className="App">
+//         <PersonList test={this.state.test}/>
+//       </div>
+//     );
+//   }
+  
+// }
+
+// export default App;
+// kurang dari 5 merah 6 - 9 kuning lebih dari 10 hijau 
+import './App.css';
+import React, { useState } from 'react'
+
+
+
+export default function App (){
+
+const [state, setState] = useState({
+  user:'',
+  password:''
+})
+
+  const onSubmit = (e) =>{
+    if(state.user === ''){
+    alert('username kosong')
+    }else{
+    alert(`username : ${state.user} Password : ${state.password}`)
+    }
+  }
+
+  const handelOnChange = e =>{
+   state[e.target.name] = e.target.value
+   setState({...state})
+  };
+
+// console.log(state)
+
+  return( 
+    <div className="container" style={{textAlign:'center', border: '2px black solid' }}>
+      <div className="header Form">
+        <p className="login">Login</p>
+      </div>
+      <div className="Form" >
+        <form>
+          <li>
+            <label className="form" >User Name</label>
+            <input id="user"  type="text"  name="user" onChange={handelOnChange} ></input>
+          </li>
+          <li>
+            <label>Password</label>
+            <input id="pas" type="password" autoComplete="password"  name="password" onChange={handelOnChange}></input>
+          </li>
+          <li>
+            <button type="submit" onClick={onSubmit}>Click me</button>
+          </li>
+        </form>
+      </div>
+    </div>
+  )
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import App from './App'
 import { shallow } from 'enzyme'
-import PersonList from "./PersonList"
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 configure({adapter : new Adapter()})
@@ -11,23 +10,89 @@ describe('App', ()=>{
   beforeAll(()=>{
     addWrapper = shallow(<App/>)
   })
-  it('test',()=>{
-     addWrapper.find(PersonList);
+
+  it ('user kosong',() =>{
+    addWrapper.find('button').simulate('click',{target:{name:{value:''}}})
+
   })
+  it("user tidak boleh kosong !", () => {
+    const onMock = jest.fn();
+    const wrap = shallow(<App onClick={onMock} />);
+    const user = wrap.find("#user");
+    user.simulate("change", { target: { value:""} });
+    const button = wrap.find("button");
+    button.simulate("submit");
+    expect(onMock).not.toHaveBeenCalled()
+  });
+    // addWrapper.find('button').simulate('click')
+    // const onMock = jest.fn()
+    // const pdSpy = jest.fn()
+    // addWrapper.shallow(<App onSubmit={onMock}/>)
+    //  addWrapper.find('button').simulate('click')
+    // expect(pdSpy).not.toHaveBeenCalled()
+    // expect(onMock).toHaveBeenCalled()
+  // // it('test',()=>{
+  // //    addWrapper.find(PersonList);
+  // // })
 
-  it('satet',()=>{
-    const stateNotNull  = addWrapper.state();
-    expect(stateNotNull).not.toBeNull();
-  })
+  // // it('satet',()=>{
+  // //   const stateNotNull  = addWrapper.state();
+  // //   expect(stateNotNull).not.toBeNull();
+  // // })
 
-  it('has a test propperty on state', () =>{
-    const appState = addWrapper.state();
-    expect(appState.test).toBeDefined()
+  // // it('has a test propperty on state', () =>{
+  // //   const appState = addWrapper.state();
+  // //   expect(appState.test).toBeDefined()
 
-  } )
+  // // } )
 
-  it('passes test property of state to personList as prop', () =>{
-    const list = addWrapper.find(PersonList);
-    expect(list.props().test).toEqual(addWrapper.state().test, console.log(test))
-  })
+  // // it('passes test property of state to personList as prop', () =>{
+  // //   const list = addWrapper.find(PersonList);
+  // //   expect(list.props().test).toEqual(addWrapper.state().test, console.log(test))
+  // // })
+
+  // it('mengembalikan merah jika nilai 1-5 ', () =>{
+  //   const lampu = App.prototype.ceklampu(1);
+  //   expect(lampu).toBe('merah')
+  // })
+  // it('mengembalikan merah jika nilai 1-5', () =>{
+  //   const lampu = App.prototype.ceklampu(2);
+  //   expect(lampu).toBe('merah')
+  // })
+  // it('mengembalikan merah jika nilai 1-5 ', () =>{
+  //   const lampu = App.prototype.ceklampu(3);
+  //   expect(lampu).toBe('merah')
+  // })
+  // it('mengembalikan merah jika nilai 1-5 ', () =>{
+  //   const lampu = App.prototype.ceklampu(4);
+  //   expect(lampu).toBe('merah')
+  // })
+  // it('mengembalikan merah jika nilai 1-5 ', () =>{
+  //   const lampu = App.prototype.ceklampu(5);
+  //   expect(lampu).toBe('merah')
+  // })
+  // it('mengembalikan kuning jika nilai 6-9 ', () =>{
+  //   const lampu = App.prototype.ceklampu(6);
+  //   expect(lampu).toBe('kuning')
+  // })
+  // it('mengembalikan kuning jika nilai 6-9 ', () =>{
+  //   const lampu = App.prototype.ceklampu(7);
+  //   expect(lampu).toBe('kuning')
+  // })
+  // it('mengembalikan kuning jika nilai 6-9 ', () =>{
+  //   const lampu = App.prototype.ceklampu(8);
+  //   expect(lampu).toBe('kuning')
+  // })
+  // it('mengembalikan kuning jika nilai 6-9 ', () =>{
+  //   const lampu = App.prototype.ceklampu(9);
+  //   expect(lampu).toBe('kuning')
+  // })
+  // it('mengembalikan hijau jika nilai 10 ', () =>{
+  //   const lampu = App.prototype.ceklampu(10);
+  //   expect(lampu).toBe('hijau')
+  // })
+  // it('mengembalikan hijau jika nilai 11 ', () =>{
+  //   const lampu = App.prototype.ceklampu(11);
+  //   expect(lampu).toBe('hijau')
+  // })
 })
